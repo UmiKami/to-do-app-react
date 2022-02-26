af99f684289af98ff6b96851921d5e9c8661fc3c
umikami <ggoku02@gmail.com>
2022-02-26 13:52:30 -0500

    implemented todos API and cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,78 @@
-import { useState } from 'react';
-import './styles/App.css'
+import { useEffect, useState } from 'react';
+import './styles/App.css';
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
 
 // Components
 import Clock from './components/Clock';
-import TaskInput from "./components/TaskInput";
-import Task from "./components/Task"
+import TaskInput from './components/TaskInput';
+import Task from './components/Task';
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
-  const [tasks, setTasks] = useState([])
+	const [ tasks, setTasks ] = useState([]);
+	const [ hasLoaded, setHasLoaded ] = useState(false);
 
-  return <>
-    <Clock/>
-    <main className="main-content">
-      <h1 className="app-name">To-Do App</h1>
-       <TaskInput setInputValue={setInputValue} inputValue={inputValue} setTasks={setTasks} tasks={tasks}/>
-       <SimpleBar className="tasks" autoHide={false}>
-       {
-        //  Loop thru tasks array [] which contains objects
-         tasks.map(({id, text, completed})=>(
-            <>
-              <Task key={id} id={id} text={text} comepleted={completed} setTasks={setTasks} tasks={tasks}/>
-            </>
-         ))
-       }
+	let counter = 0;
 
-       </SimpleBar>
-    </main>    
-  </>
+	useEffect(() => {
+		if (!hasLoaded) {
+			setHasLoaded(true);
+
+			fetch('http://assets.breatheco.de/apis/fake/todos/user/umikami', {
+				method: 'GET',
+				headers: {}
+			})
+				.then((response) => {
+					return response.json();
+				})
+				.then((result) => {
+					counter++;
+					console.log(counter);
+					if (JSON.stringify(result) !== JSON.stringify(tasks)) {
+						setTasks(result);
+					}
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
+
+		fetch('http://assets.breatheco.de/apis/fake/todos/user/umikami', {
+			method: 'PUT',
+
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(tasks)
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((result) => {
+				console.log(result);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, [hasLoaded, tasks, counter]);
+
+	return (
+		<>
+			<Clock />
+			<main className="main-content">
+				<h1 className="app-name">To-Do App</h1>
+				<TaskInput setTasks={setTasks} tasks={tasks} />
+				<SimpleBar className="tasks" autoHide={false}>
+					{//  Loop thru tasks array [] which contains objects
+					tasks.map(({ key, id, label, done }) => (
+						<>
+							<Task key={key} id={id} label={label} done={done} setTasks={setTasks} tasks={tasks} />
+						</>
+					))}
+				</SimpleBar>
+			</main>
+		</>
+	);
 }
 
 export default App;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,14 @@
 import '../styles/Task.css';
 
-const Task = ({ key, id, text, completed, tasks, setTasks }) => {
+const Task = ({ id, label, done, tasks, setTasks }) => {
 	const deleteCurrentTask = () => {
 		let newTaskList = tasks.filter((task) => task.id !== id);
 		setTasks(newTaskList);
 	};
 
 	return <>
-	<section className="task-container" key={key}>
-			<p className="task-text-content">{text}</p>
+	<section className="task-container" key={id}>
+			<p className="task-text-content">{label}</p>
 			<div className="buttons">
 				<div className="calendar-btn">
 					<i className="fa-solid fa-calendar-days" />
